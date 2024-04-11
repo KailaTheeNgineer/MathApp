10ff6273b67c3454f60b81a97c41b79ad2e9dc70
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-04-11 13:48:15 -0400

    PracticeQuestions

---
+++ src/main/java/com/Pluralsight/MathApp.java
@@ -0,0 +1,31 @@
+package com.Pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        int bobSalary = 600;
+        int garySalary = 300;
+        int HighestSalary = Math.max(bobSalary, garySalary);
+                        System.out.println("The highest salary is " + HighestSalary);
+        int carPrice = 1000;
+        int truckPrice = 5000;
+        int minPrice = Math.min(carPrice, truckPrice);
+                        System.out.print(minPrice);
+        double circleRadius = 7.25;
+        double circleArea = circleRadius * 9.86;
+                        System.out.println(circleArea);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
